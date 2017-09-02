94db9cd977543080a073860011535836732ef195
concept-x <emilie_elliotthotmail.com>
2017-09-02 18:12:52 -0500

    angular assignment 5: completed

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -2,7 +2,7 @@
     function CollectionCtrl() {
       this.albums = [];
       for (var i=0; i<12; i++){
-        this.albums.push(angular.copy(album.Picasso));
+        this.albums.push(angular.copy(albumPicasso));
       }
     }
 
