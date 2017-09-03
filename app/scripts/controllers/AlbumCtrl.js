860f37dc5a4762c81166196f7cd56298efd18c02
concept-x <emilie_elliotthotmail.com>
2017-09-02 22:12:00 -0500

    checkpoint 6: completed

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,49 +1,11 @@
-/*
--Add an albumData property that holds a copy of albumPicasso.
--Use ngRepeat on the album-view-song-item table row to add a song row for each song on the album. Replace the static song information with the song data using the corresponding scope properties and {{ }} markup:
---number (Refer to the table of "exposed properties" in the ngRepeat documentation)
---name
---length (You'll filter the time code in a later checkpoint)
--In the Album template, replace the static album information with the album data using {{ }} markup:
---album art
---name
---artist
---year and record label
-*/
 
 (function(){
-  function AlbumCtrl(){
-    this.albumData = angular.copy(albumPicasso);
+  function AlbumCtrl(Fixtures){//inject 'Fixtures' custom service
+    this.albumData = Fixtures.getAlbum();
+    //AlbumCtrl uses Fixtures' getAlbum() method to get albumPicasso object
   }
 
 angular
     .module('blocJams')
-    .controller('AlbumCtrl', AlbumCtrl)
+    .controller('AlbumCtrl',['Fixtures', AlbumCtrl]);//add 'Fixtures' to AlbumCtrl's array of dependencies
 })();
-
-/*
-//CollectionCtrl.js
-(function() {
-    function CollectionCtrl() {
-      this.albums = [];
-      for (var i=0; i<12; i++){
-        this.albums.push(angular.copy(album.Picasso));
-      }
-    }
-
-    angular
-        .module('blocJams')
-        .controller('CollectionCtrl', CollectionCtrl);
-})();
-
-//LandingCtrl.js
-(function() {
-    function LandingCtrl() {
-      this.heroTitle = "Turn the Music Up!"
-    }
-
-    angular
-        .module('blocJams')
-        .controller('LandingCtrl', LandingCtrl);
-})();
-*/
