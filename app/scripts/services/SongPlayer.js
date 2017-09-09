ae262097bcac1bd7208f1836cafe42b568775c24
concept-x <emilie_elliotthotmail.com>
2017-09-09 18:26:54 -0500

    checkpoint 8: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,9 +1,13 @@
 //SongPlayer is a service
 (function() {
-    function SongPlayer() {
+    function SongPlayer(Fixtures) {
          var SongPlayer = {};
 
-         var currentSong = null;
+         /*
+         * @desc currentAlbum from Fixtures
+         * @type {Object}???
+         */
+         var currentAlbum = Fixtures.getAlbum();
          /*
          * @desc Buzz object audio file
          * @type {Object}
@@ -17,7 +21,7 @@
          var setSong = function(song){
            if (currentBuzzObject){
              currentBuzzObject.stop();
-             currentSong.playing = null;
+             SongPlayer.currentSong.playing = null;
            }
 
            currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -25,7 +29,7 @@
              preload: true
            });
 
-           currentSong = song;
+           SongPlayer.currentSong = song;
          };
 
          /*
@@ -36,19 +40,36 @@
           var playSong = function(song){
               currentBuzzObject.play();
               song.playing = true;
-              }
+          }
+
+          /*
+           * @function getSongIndex
+           * @desc Returns current song index
+           * @param {Object} song
+           * @returns song index
+           */
+          var getSongIndex = function(song){
+              return currentAlbum.songs.indexOf(song);
+          };
+
+          /*
+           * @desc Active song object from list of songs
+           * @type {Object}
+           */
+          SongPlayer.currentSong = null;
         /*
-         * @function SongPlayer.play
+         * @function play
          * @desc Plays currentSong from either none playing or paused
          */
          SongPlayer.play = function(song){
            //add 'play' method to SongPlayer; 'play' takes 'song' argument
-           if(currentSong !== song){
+           song = song || SongPlayer.currentSong;
+           if(SongPlayer.currentSong !== song){
            setSong(song);
            playSong(song);//replace two lines below with playSong function
            //currentBuzzObject.play();
            //song.playing = true;
-           } else if(currentSong === song){
+           } else if(SongPlayer.currentSong === song){
              if(currentBuzzObject.isPaused()){
                playSong(song);
                 //currentBuzzObject.play();
@@ -58,12 +79,31 @@
          };
 
     /*
-     * @function SongPlayer.pause
-     * @desc Pauses playing currentBuzzObject when clicked
+     * @function pause
+     * @desc Pauses current song
      */
      SongPlayer.pause = function(song){
+     song = song || SongPlayer.currentSong;
      currentBuzzObject.pause();
      song.playing = false;
+   };
+
+   /*
+    * @function previous
+    * @desc rew to prev song index
+    */
+   SongPlayer.previous = function(){
+     var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+     currentSongIndex--;
+
+     if(currentSongIndex < 0){
+       currentBuzzObject.stop();
+       SongPlayer.currentSong.playing = null;
+     } else {
+        var song = currentAlbum.songs[currentSongIndex];
+        setSong(song);
+        playSong(song);
+     }
    };
          return SongPlayer;
     }
@@ -73,5 +113,5 @@
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);//'factory' is a recipe...???
+        .factory('SongPlayer', ['Fixtures', SongPlayer]);//'factory' is a recipe...???
 })();
